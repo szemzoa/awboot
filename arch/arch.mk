efbc00083f9416b3ff81f29f08bca668093dc971
Fabien Poussin <fabien.poussin@gmail.com>
2023-01-07 16:07:13 +0100

    makefiles: add conditional building for peripherals

--- arch/arch.mk
+++ arch/arch.mk
@@ -5,16 +5,28 @@ INCLUDE_DIRS += -I $(ARCH)/arm32/include -I $(SOC)/include -I $(SOC) -I $(SOC)/m
 
 CFLAGS += -DCOUNTER_FREQUENCY=24000000
 
+ASRCS	+=  $(SOC)/start.S
+ASRCS	+=  $(SOC)/memcpy.S
+
+ifneq ($(wildcard $(SOC)/dram_sun20i_d1.c),)
+SRCS	+=  $(SOC)/dram_sun20i_d1.c
+else
 SRCS	+=  $(SOC)/dram.c
+endif
 SRCS	+=  $(SOC)/sunxi_usart.c
 SRCS	+=  $(SOC)/arch_timer.c
-SRCS	+=  $(SOC)/sunxi_spi.c
-SRCS	+=  $(SOC)/sunxi_dma.c
 SRCS	+=  $(SOC)/sunxi_gpio.c
-SRCS	+=  $(SOC)/sdmmc.c
-SRCS	+=  $(SOC)/sunxi_sdhci.c
 SRCS	+=  $(SOC)/sunxi_clk.c
 SRCS	+=  $(SOC)/exception.c
 
-ASRCS	+=  $(SOC)/start.S
-ASRCS	+=  $(SOC)/memcpy.S
+USE_SPI = $(shell grep -E "^\#define CONFIG_BOOT_SPI" board.h)
+ifneq ($(USE_SPI),)
+SRCS	+=  $(SOC)/sunxi_spi.c
+SRCS	+=  $(SOC)/sunxi_dma.c
+endif
+
+USE_SDMMC = $(shell grep -E "^\#define CONFIG_BOOT_(SDCARD|MMC)" board.h)
+ifneq ($(USE_SDMMC),)
+SRCS	+=  $(SOC)/sdmmc.c
+SRCS	+=  $(SOC)/sunxi_sdhci.c
+endif
